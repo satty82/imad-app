44e613a179e18fdd5c963bf47cb6aa6ff9e01965
satty82 <satyamraikwar82@gmail.com>
2017-08-27 02:44:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ var htmlTemplate=`
 
 app.get('/:articleName',function (req,res){
     var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName()]));
+  res.send(createTemplate(articles[articleName]));
 });
 
 
