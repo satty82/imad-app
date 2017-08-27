7027e2fe5dc64405ddaae86242444ec475871802
satty82 <satyamraikwar82@gmail.com>
2017-08-27 17:00:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ var element= document.getElementById('main-text');
 element.innerHTML='New Value';
 
 var img=document.getElementById("madi");
+
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+5;
+     img.style.marginLeft=marginLeft+"px";
+     
+}
 img.onclick=function(){
-  img.style.marginLeft='100px';  
+var interval=setInterval(moveRight,50);
 };
