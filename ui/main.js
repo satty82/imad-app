1b998f4d1e4e921ea89e05e6c92c9f91680b2605
satty82 <satyamraikwar82@gmail.com>
2017-08-28 02:53:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,13 @@ function moveRight(){
 img.onclick=function(){
 var interval=setInterval(moveRight,50);
 };
+
+var button=document.getElementById("counter");
+var counter=0;
+
+button.onclick= function(){
+    
+    counter=counter+1;
+var span=document.getElementById("count");
+span.innerHTML=counter.toString();
+};
