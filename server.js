c2e19e3d34579e74c89f47158b33cd4d841626d6
satty82 <satyamraikwar82@gmail.com>
2017-09-10 21:39:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ var htmlTemplate=`
       <body>
        <div class="container">
             <div>
-           <font color="red"> <a href="/">HOME</a>
+           <font face="verdana" color="red"> <a href="/">HOME</a></font>
           </div>
           <hr/>
           <h3>${heading}</h3>
